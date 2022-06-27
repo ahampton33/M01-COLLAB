9cb163b29ca3c1ce45b7b1e497dacb0327fd7676
ahampton33 <103609980+ahampton33@users.noreply.github.com>
2022-06-27 19:01:10 -0400

    Add files via upload

---
+++ practice_function.py
@@ -0,0 +1,21 @@
+# for n in range(10):
+#     if n % 2 != 0:
+#         print(n)
+
+
+#M03 LAB
+vehicle = input("Please enter a vehicle type: ")
+
+
+year = input("Please enter the year of the vehicle: ")
+make = input("Please enter the make of the vehicle: ")
+model = input("Please enter the model of the vehicle: ")
+door_num = input("Please enter the number of doors on the vehicle: ")
+roof = input("Please enter if the vehicle has a sun roof or not: ")
+
+print(f"Automobile: {vehicle}")
+print(f"Year: {year}")
+print(f"Make: {make}")
+print(f"Model: {model}")
+print(f"Number of doors: {door_num}")
+print(f"Type of roof: {roof}")
